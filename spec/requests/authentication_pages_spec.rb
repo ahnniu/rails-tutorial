60f2665ad610939d5f96749cd1813135fc96c32f
Yongjia Niu <ahnniu@gmail.com>
2016-01-19 09:23:56 +0800

    'with valid information' 登录测试失败的原因是: 数据库写入的是 小写的email, 登录表单里是大写,匹配不成功,所以失败.
    如果测试比较难定位的话,可以到 log/test.log里看

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -33,7 +33,7 @@ describe 'Authentication' do
         @user = User.create(name: "Example User", email: "user@example.com",
                       password: "foobar", password_confirmation: "foobar")
 
-        fill_in 'Email', with: @user.email.upcase
+        fill_in 'Email', with: @user.email
         fill_in 'Password', with: @user.password
         click_button 'Sign in'
       end
