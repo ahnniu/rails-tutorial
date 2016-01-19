bce2f9e27d41b83befe27524ffae13602fcd9ad3
Yongjia Niu <ahnniu@gmail.com>
2016-01-19 09:46:06 +0800

    Sqlite数据库查找时区分大小写,所以,当提交表单email写的是大写时候, 测试失败(因为在执行SQL查询时,因区分大小写,未找到记录), 所以,在sessions controller create时,先把获取表单的email转为小写,再执行sql查询

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -32,9 +32,7 @@ describe 'Authentication' do
       let(:user) { FactoryGirl.create(:user) }
 
       before do
-        # @user = User.create(name: "Example User", email: "user@example.com",
-        #               password: "foobar", password_confirmation: "foobar")
-        fill_in 'Email', with: user.email
+        fill_in 'Email', with: user.email.upcase
         fill_in 'Password', with: user.password
         click_button 'Sign in'
       end
