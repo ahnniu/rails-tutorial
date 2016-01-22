865e1a3d4d072180b4f8eae7ba088bcc3ac90824
Yongjia Niu <ahnniu@gmail.com>
2016-01-22 15:01:50 +0800

    Finish 9.2.3 登录后跳转到先前访问的受保护页面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,11 +43,12 @@ private
   end
 
   def signed_in_user
+    store_location
     redirect_to signin_url, notice: 'Please sign in.' unless signed_in?
   end
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to root_url, notice: 'You are not ' unless current_user?(@user)
+    redirect_to root_url unless current_user?(@user)
   end
 end
