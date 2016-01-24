85ce90913517dfd1863620643d88c509344e80c7
Yongjia Niu <ahnniu@gmail.com>
2016-01-24 13:56:36 +0800

    虽然在spec_helper.rb中都有require spec/support中的代码（见spec_helper.rb代码），一般情况下，仅仅require 'spec_helper'即可，但是在support子目录里，有时候因为require文件顺序不同，造成 spec/support/utilities 找不到符号，那么require即可。

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,6 @@
+require Rails.root.join('spec/support/flash_spec_helper.rb')
+require Rails.root.join('spec/support/authentication_spec_help.rb')
+
 include ApplicationHelper
 include FlashSpecHelper
 include AuthenticationSpecHelp
